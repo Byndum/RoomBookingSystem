842d772d6b7ad3523006692cbda97f0076cab30e
Byndum <Mikael.List-Jensen@hotmail.dk>
2024-05-27 15:12:42 +0200

    changed to another java 18.0.2

--- src/main/java/com/example/roombookingsystem/application/controller/LoginController.java
+++ src/main/java/com/example/roombookingsystem/application/controller/LoginController.java
@@ -1,11 +1,15 @@
 package com.example.roombookingsystem.application.controller;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 
 public class LoginController {
 
     public void btnLogin(MouseEvent mouseEvent) {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/example/roombookingsystem/home-view.fxml"));
+        Scene scene = new Scene(root, 1280, 720);
         System.out.println("Login");
     }
 }
