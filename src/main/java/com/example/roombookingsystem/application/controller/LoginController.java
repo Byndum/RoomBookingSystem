135ea96ccc803e8100d81ab71deb7346fbfd3d7f
Byndum <Mikael.List-Jensen@hotmail.dk>
2024-05-27 15:55:38 +0200

    Login button (view switch)

--- src/main/java/com/example/roombookingsystem/application/controller/LoginController.java
+++ src/main/java/com/example/roombookingsystem/application/controller/LoginController.java
@@ -1,15 +1,25 @@
 package com.example.roombookingsystem.application.controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
+    @FXML
+    Button btnLogin;
 
-    public void btnLogin(MouseEvent mouseEvent) {
+    public void btnLoginClick(MouseEvent mouseEvent) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/com/example/roombookingsystem/home-view.fxml"));
         Scene scene = new Scene(root, 1280, 720);
-        System.out.println("Login");
+        Stage stage = (Stage) btnLogin.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+        stage.centerOnScreen();
     }
 }
