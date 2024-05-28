c969ef1eed430abf57255cf56e4b5cbcc4a95c21
Byndum <Mikael.List-Jensen@hotmail.dk>
2024-05-28 11:20:33 +0200

    added menu tabs, and changed their color

--- src/main/java/com/example/roombookingsystem/application/controller/HomeController.java
+++ src/main/java/com/example/roombookingsystem/application/controller/HomeController.java
@@ -11,21 +11,43 @@ import javafx.scene.paint.Paint;
 public class HomeController {
     @FXML
     private StackPane menuHome;
+    @FXML
+    private StackPane menuBook;
+    @FXML
+    private StackPane menuEdit;
+    @FXML
+    private StackPane menuHistory;
+    @FXML
+    private StackPane menuRegisterRoom;
+    @FXML
+    private StackPane menuRegisterError;
+    @FXML
+    private StackPane menuAllErrors;
 
     @FXML
     public void initialize() {
+        menuHome.setStyle("-fx-background-color: #00adef");
+        //menuHome.setStyle("-fx-background-color: linear-gradient(to right, #00adef, #f4f4f4)");
+        menuBook.setStyle("-fx-background-color: #00adef");
+        menuEdit.setStyle("-fx-background-color: #00adef");
+        menuHistory.setStyle("-fx-background-color: #00adef");
+        menuRegisterRoom.setStyle("-fx-background-color: #00adef");
+        menuRegisterError.setStyle("-fx-background-color: #00adef");
+        menuAllErrors.setStyle("-fx-background-color: #00adef");
+
         menuHome.setOnMouseClicked(mouseEvent -> {
             System.out.println("pane has been clicked!");
         });
 
         menuHome.setOnMouseEntered(mouseEvent -> {
             System.out.println("hover");
-            menuHome.setStyle("-fx-background-color: #000");
+            menuHome.setStyle("-fx-background-color: #00bfff");
         });
 
         menuHome.setOnMouseExited(mouseEvent -> {
             System.out.println("no longer hover");
-            menuHome.setStyle("-fx-background-color: #fff");
+            menuHome.setStyle("-fx-background-color: #00adef");
+            //menuHome.setStyle("-fx-background-color: #fcfcfc");
         });
     }
 
