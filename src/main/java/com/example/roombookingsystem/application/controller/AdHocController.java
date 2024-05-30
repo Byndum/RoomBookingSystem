cd990c0dbeee9af839b620282af362a88e8b9886
jakub <jakubsmail8@gmail.com>
2024-05-30 13:40:18 +0200

    test

--- src/main/java/com/example/roombookingsystem/application/controller/AdHocController.java
+++ src/main/java/com/example/roombookingsystem/application/controller/AdHocController.java
@@ -1,12 +1,16 @@
 package com.example.roombookingsystem.application.controller;
 
+import com.example.roombookingsystem.foundation.ObservableBooking;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
 
-public class AdHocController {
+import java.util.ArrayList;
 
+public class AdHocController {
 
     @FXML
     Pane MainPane;
@@ -15,18 +19,24 @@ public class AdHocController {
     @FXML
     Button filterButton;
     @FXML
-    TableView RoomsTableView;
+    TableView<String> RoomsTableView;
+
+
+
 
     public void initialize() {
+
         filterMenu.setVisible(false);
         filterButton.setLayoutY(370);
         RoomsTableView.setPrefHeight(340);
 
-        filterButton.setOnMouseClicked(event -> {
-            filterMenu.setVisible(!filterMenu.isVisible());
-            if (filterMenu.isVisible()) {
+
+        filterButton.setOnMouseClicked(event -> { // test
+            boolean isVisible = !filterMenu.isVisible();
+            filterMenu.setVisible(isVisible);
+            if (isVisible) {
                 filterButton.setLayoutY(270);
-                RoomsTableView.setPrefHeight(240);
+                RoomsTableView.setPrefHeight(270);
             } else {
                 filterButton.setLayoutY(370);
                 RoomsTableView.setPrefHeight(340);
