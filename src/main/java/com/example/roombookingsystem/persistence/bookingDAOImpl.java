7e529a5ec24da3cc21bad42354ca90e3b69ec5c5
SomeoneNamedMarc <thatguymarc0504@gmail.com>
2024-05-30 10:46:54 +0200

    Added overloading for Booking object and preparedstatements for better security

--- src/main/java/com/example/roombookingsystem/persistence/bookingDAOImpl.java
+++ src/main/java/com/example/roombookingsystem/persistence/bookingDAOImpl.java
@@ -31,12 +31,46 @@ public class bookingDAOImpl implements bookingDAO {
 
     @Override
     public Booking getBookingByID(int id) {
-        return null;
+        String query = "SELECT * FROM tblBooking WHERE fldBookId = ?";
+        try (Connection connection = databaseConnection.getInstance();
+             PreparedStatement pstmt = connection.prepareStatement(query)) {
+            pstmt.setInt(1, id);
+
+            ResultSet resultSet = pstmt.executeQuery();
+            Booking booking = new Booking();
+            while (resultSet.next())
+            {
+                booking.setBookingID(resultSet.getInt(1));
+                booking.setTitle(resultSet.getString(2));
+                booking.setDate(resultSet.getDate(3));
+                booking.setTime(resultSet.getTime(4));
+                booking.setTimeEnd(resultSet.getTime(5));
+                booking.setCatering(resultSet.getBoolean(6));
+                booking.setRoomID(resultSet.getInt(7));
+                booking.setUserID(resultSet.getInt(8));
+
+            }
+            resultSet.close();
+            connection.close();
+
+            return booking;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void deleteBooking(int id) {
+        String query = "DELETE FROM tblBooking WHERE fldBookId = ?";
+        try (Connection connection = databaseConnection.getInstance();
+             PreparedStatement pstmt = connection.prepareStatement(query)) {
+            pstmt.setInt(1, id);
 
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+        throw new RuntimeException(e);
+        }
     }
 
     public void updateTodaysBookingsArrayList() {
@@ -58,6 +92,7 @@ public class bookingDAOImpl implements bookingDAO {
                         resultSet.getInt(8)
                 ));
             }
+            resultSet.close();
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -70,16 +105,30 @@ public class bookingDAOImpl implements bookingDAO {
 
 
     @Override
-    public void updateBooking(Booking booking)
-    {
+    public void updateBooking(Booking booking) {
         String query = "UPDATE tblBooking SET " +
-                "fldRoomID = (SELECT fldRoomID FROM tblRoom WHERE fldRoomName = " + booking.getRoomID() + "), " +
-                "fldDate = " + booking.getDate() + ", " +
-                "fldTimeStart = " + booking.getTime() + ", " +
-                "fldTimeEnd = " + booking.getTimeEnd() + ", " +
-                "fldCatering = " + booking.isCatering() + " " +
-                "WHERE fldBookId = " + booking.getBookingID();
+                "fldRoomID = (SELECT fldRoomID FROM tblRoom WHERE fldRoomName = ?), " +
+                "fldDate = ?, " +
+                "fldTimeStart = ?, " +
+                "fldTimeEnd = ?, " +
+                "fldCatering = ?, " +
+                "fldUserID = ?" +
+                " WHERE fldBookId = ?";
 
-        System.out.println(query);
+        try (Connection connection = databaseConnection.getInstance();
+             PreparedStatement pstmt = connection.prepareStatement(query)) {
+            pstmt.setInt(1, booking.getRoomID());
+            pstmt.setDate(2, booking.getDate());
+            pstmt.setTime(3, booking.getTime());
+            pstmt.setTime(4, booking.getTimeEnd());
+            pstmt.setBoolean(5, booking.isCatering());
+            pstmt.setInt(6, booking.getUserID());
+            pstmt.setInt(7, booking.getBookingID());
+
+            pstmt.execute();
+
+        } catch (SQLException e) {
+        throw new RuntimeException(e);
+        }
     }
 }
