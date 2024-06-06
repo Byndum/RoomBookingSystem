53edb7e62962cbe4775bcc8ca062c515fa28c40c
SomeoneNamedMarc <thatguymarc0504@gmail.com>
2024-06-06 14:06:30 +0200

    Added attributes for equipments and fault-info

--- src/main/java/com/example/roombookingsystem/persistence/GenericQuerries/DBRooms.java
+++ src/main/java/com/example/roombookingsystem/persistence/GenericQuerries/DBRooms.java
@@ -13,8 +13,23 @@ public class DBRooms {
 
     public ArrayList<Room> getAllRooms() {
         Connection connection = databaseConnection.getInstance();
-        String query = "SELECT * FROM tblRoom";
-
+        String query = "SELECT \n" +
+                "    r.fldRoomID,\n" +
+                "    r.fldRoomName,\n" +
+                "    r.fldRoomSize,\n" +
+                "    MAX(CASE WHEN re.fldEquipmentID = 1 THEN 'True' ELSE 'False' END) AS Equipment1,\n" +
+                "    MAX(CASE WHEN re.fldEquipmentID = 2 THEN 'True' ELSE 'False' END) AS Equipment2,\n" +
+                "    MAX(CASE WHEN re.fldEquipmentID = 3 THEN 'True' ELSE 'False' END) AS Equipment3,\n" +
+                "    MAX(CASE WHEN re.fldEquipmentID = 4 THEN 'True' ELSE 'False' END) AS Equipment4,\n" +
+                "    MAX(CONVERT(nvarchar(max), f.fldFaultTxt)) AS Faults\n" +
+                "FROM \n" +
+                "    tblRoom r\n" +
+                "LEFT JOIN \n" +
+                "    tblRoomEquipment re ON r.fldRoomID = re.fldRoomID\n" +
+                "LEFT JOIN \n" +
+                "    tblFault f ON r.fldRoomID = f.fldRoomID\n" +
+                "GROUP BY \n" +
+                "    r.fldRoomID, r.fldRoomName, r.fldRoomSize";
         try {
             PreparedStatement ps = connection.prepareStatement(query);
             ResultSet rs = ps.executeQuery();
@@ -22,7 +37,12 @@ public class DBRooms {
                 rooms.add(new Room(
                         rs.getInt(1),
                         rs.getString(2),
-                        rs.getInt(3)
+                        rs.getInt(3),
+                        rs.getBoolean(4),
+                        rs.getBoolean(5),
+                        rs.getBoolean(6),
+                        rs.getBoolean(7),
+                        rs.getString(8)
                 ));
             }
             return rooms;
